3953bff03a36c73cdcf403c2a40a7f7055d8584e
Isha Singhal <ishasinghal2002@gmail.com>
2024-01-20 19:50:08 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.3"
+
+REPO_NAME = "ipynb-renderer"
+AUTHOR_USER_NAME = "ishasinghal23"
+SRC_REPO = "ipynb-renderer"
+AUTHOR_EMAIL = "ishasinghal2002@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+    
